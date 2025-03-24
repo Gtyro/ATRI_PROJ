d87cfbac6719697dede3a58a8f70b1c4e42bc90f
Gtyro <1094102616@qq.com>
2025-03-24 21:08:56 +0800

    在主文件中引入Element Plus样式，以增强前端界面的视觉效果。

--- plugins/webui/frontend/src/main.js
+++ plugins/webui/frontend/src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import pinia from './stores'
 import './api/index.js'  // 引入axios配置
 
+// 引入Element Plus
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
